dcd63f7d2aad712ae6988095a8ab390d83c852ca
John B <johnbernalfsd@gmail.com>
2022-10-30 21:47:54 -0600

    Added basic initial styling
    - Imported Google Fonts
    - Formatted foundational page layout
    - Added landing page with background image & gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <div>
+    <div
+      className="text-white text-center bg-gradient-to-br from-fuchsia-700 to-violet-800 shadow-lg
+      rounded-lg p-12 max-w-screen-md w-screen h-full relative overflow-hidden
+      after:bg-transparent-geometry after:z-10 after:inset-0 after:absolute"
+    >
       <header>
-        <h1 className="text-green-500">Hello, world!</h1>
+        <h1 className="text-6xl font-bangers drop-shadow-sm shadow-amber-500">Hello, world!</h1>
+        <div className="flex flex-col">
+          <p className="">Building a brand new site</p>
+        </div>
       </header>
     </div>
   );
