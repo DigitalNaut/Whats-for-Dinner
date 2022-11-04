cbebe4c7685a09f0b624155ef91cf4ad5d742f63
John B <johnbernalfsd@gmail.com>
2022-11-04 09:18:43 -0600

    Reorganized the Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloudArrowUp } from "@fortawesome/free-solid-svg-icons";
+
 import useGoogleDrive from "src/hooks/GoogleDrive";
+import Spinner from "src/components/Spinner";
 
 export default function Main() {
   const { uploadFile, fetchFiles, isLoaded } = useGoogleDrive();
+
   const [file, setFile] = useState<File>();
   const [error, setError] = useState<string>();
   const [driveFiles, setDriveFiles] = useState<gapi.client.drive.File[]>();
+
+  const [loadingDriveFiles, setLoadingDriveFiles] = useState(true);
+  const [uploadingFile, setUpLoadingFile] = useState(false);
 
   const handleInputChange = async (
     event: React.ChangeEvent<HTMLInputElement>
@@ -17,6 +25,7 @@ export default function Main() {
 
   const uploadFileHandler = async () => {
     setError(undefined);
+    setUpLoadingFile(true);
 
     if (!file) return setError("No file selected");
 
@@ -46,24 +55,39 @@ export default function Main() {
 
             listFiles();
           }
+
+          setUpLoadingFile(false);
         }
       );
     } catch (error) {
+      setUpLoadingFile(false);
+
       if (error instanceof Error) setError(error.message);
       else if (typeof error === "string") setError(error);
-      else console.error(error);
+      else {
+        setError("An unknown error ocurred");
+        console.error(error);
+      }
     }
   };
 
   async function listFiles() {
+    setLoadingDriveFiles(true);
+
     try {
       const files = await fetchFiles();
       if (files?.length) setDriveFiles(files);
+      else setDriveFiles(undefined);
     } catch (error) {
       if (error instanceof Error) setError(error.message);
       else if (typeof error === "string") setError(error);
-      else console.error(error);
+      else {
+        setError("An unknown error ocurred");
+        console.error(error);
+      }
     }
+
+    setLoadingDriveFiles(false);
   }
 
   useEffect(() => {
@@ -75,48 +99,62 @@ export default function Main() {
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
-    <div className="flex p-6">
-      <div className="flex-1">
-        <h2 className="text-xl mb-4">File upload</h2>
-        <div className="flex flex-col gap-2">
-          <input
-            type="file"
-            onChange={handleInputChange}
-            accept="image/png, image/jpeg, image/webp"
-          />
-          <img
-            src={
-              file
-                ? URL.createObjectURL(file)
-                : "https://via.placeholder.com/150"
-            }
-            className="w-[150px] h-[150px]"
-          />
-          {error && (
-            <div className="p-2 rounded-sm w-full bg-red-500 text-white">
-              {error}
-            </div>
+    <>
+      {error && (
+        <div className="p-2 rounded-sm w-full bg-red-500 text-white">
+          {error}
+        </div>
+      )}
+      <div className="flex p-6">
+        <div className="flex-1">
+          <h2 className="text-xl mb-4">File upload</h2>
+          <div className="flex flex-col gap-2">
+            <input
+              type="file"
+              onChange={handleInputChange}
+              accept="image/png, image/jpeg, image/webp"
+            />
+            {file && (
+              <>
+                <img
+                  src={
+                    file
+                      ? URL.createObjectURL(file)
+                      : "https://via.placeholder.com/150"
+                  }
+                  className="w-[150px] h-[150px] rounded-md"
+                />
+                <button
+                  data-filled
+                  onClick={uploadFileHandler}
+                  disabled={uploadingFile}
+                  className="flex gap-2 items-center"
+                >
+                  {uploadingFile ? (
+                    <Spinner text="Uploading..." />
+                  ) : (
+                    <>
+                      <FontAwesomeIcon icon={faCloudArrowUp} /> Upload
+                    </>
+                  )}
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="flex-1">
+          <h2 className="text-xl mb-4">Files saved</h2>
+          {driveFiles ? (
+            driveFiles.map((file) => <div key={file.id}>{file.name}</div>)
+          ) : (
+            <div>No files found</div>
           )}
-          <button
-            data-filled
-            onClick={uploadFileHandler}
-            disabled={file === undefined}
-          >
-            Upload
+          <button data-filled onClick={listFiles} disabled={loadingDriveFiles}>
+            {loadingDriveFiles ? <Spinner /> : "Refresh list"}
           </button>
         </div>
       </div>
-      <div className="flex-1">
-        <h2 className="text-xl mb-4">Files saved</h2>
-        {driveFiles ? (
-          driveFiles.map((file) => <div key={file.id}>{file.name}</div>)
-        ) : (
-          <div>No files found</div>
-        )}
-        <button data-filled onClick={listFiles}>
-          Refresh list
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
