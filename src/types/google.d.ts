93ca00f25509cbe7eb8c9891cf389fbf96eb968b
John B <johnbernalfsd@gmail.com>
2022-11-04 08:00:40 -0600

    Updated Google type declarations

--- src/types/google.d.ts
+++ src/types/google.d.ts
@@ -11,3 +11,28 @@ declare type GoogleUserInfo =
     }
   | null
   | undefined;
+
+type FileUploadError = {
+  error: {
+    errors: [
+      {
+        domain: string;
+        reason: string;
+        message: string;
+      }
+    ];
+    code: number;
+    message: string;
+  };
+};
+
+type FileUploadSuccess = {
+  kind: string;
+  id: string;
+  name: string;
+  mimeType: string;
+};
+
+declare type FileUploadJSONResponse =
+  | (FileUploadError & FileUploadSuccess)
+  | false;
