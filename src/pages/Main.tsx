e913ce1011fae97cea55dbc1130d3dab2d4e6842
John B <johnbernalfsd@gmail.com>
2022-11-10 11:39:49 -0600

    Changed the layout of the Main page
    - Added a mock history gallery
    - Added the Roulette component
    - Added a useless floating action button

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,31 +1,88 @@
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
-import { useGoogleDrive } from "src/hooks/GoogleDriveContext";
-import Spinner from "src/components/Spinner";
-import ImageUpload from "src/components/ImageUpload";
-import ImageList from "src/components/ImageList";
+import Roulette from "src/components/Roulette";
+
+const history: { name: string; imageUrl: string }[] = [
+  {
+    name: "Tacos al pastor",
+    imageUrl:
+      "https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Pozole", // c-spell-checker:disable-line
+    imageUrl:
+      "https://images.unsplash.com/photo-1649532245300-c3ed0565ffa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Ensalada de atún",
+    imageUrl:
+      "https://images.unsplash.com/photo-1612949060306-4c298ad7f34c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Sopa de pollo",
+    imageUrl:
+      "https://images.unsplash.com/photo-1569058242276-0bc3e078cf86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Ensalada verde",
+    imageUrl:
+      "https://images.unsplash.com/photo-1608032077018-c9aad9565d29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Carne asada",
+    imageUrl:
+      "https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Pizza",
+    imageUrl:
+      "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Hamburguesa",
+    imageUrl:
+      "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Pollo empanizado", // c-spell-checker:disable-line
+    imageUrl:
+      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    name: "Arroz con pollo",
+    imageUrl:
+      "https://images.unsplash.com/photo-1569058242252-623df46b5025?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+];
 
 export default function Main() {
-  const { isLoaded } = useGoogleDrive();
-  const [refreshDate, setRefreshDate] = useState(Date.now());
-
-  if (!isLoaded)
-    return (
-      <div className="absolute inset-0 grid place-items-center">
-        <Spinner />
-      </div>
-    );
-
   return (
-    <div className="flex">
-      <div className="w-1/2 p-2">
-        <h2 className="text-xl mb-4">Guardar imagen</h2>
-        <ImageUpload onUpload={() => setRefreshDate(Date.now())} />
+    <div className="flex flex-col w-full gap-8">
+      <Roulette />
+
+      <div className="flex p-2 gap-4 min-w-full overflow-x-auto bg-slate-700 ">
+        {history.map((dish) => (
+          <div className="group" key={dish.name}>
+            <div className="relative w-16 md:w-24 lg:w-28 aspect-square rounded-lg bg-gray-700 overflow-hidden">
+              <span className="absolute text-center text-sm w-full h-full m-auto hidden group-hover:block bg-black/50 pointer-events-none">
+                {dish.name}
+              </span>
+              <img
+                className="w-full h-full object-cover"
+                src={dish.imageUrl}
+                alt={dish.name}
+              />
+            </div>
+          </div>
+        ))}
       </div>
 
-      <div className="w-1/2 p-2 flex flex-col gap-4">
-        <h2 className="text-xl mb-4">Imágenes guardadas</h2>
-        <ImageList refreshDate={refreshDate} />
+      <div className="fixed left-1/2 -translate-x-1/2 bottom-16 flex items-center w-fit">
+        <button data-filled>
+          <FontAwesomeIcon icon={faEdit} />
+          <span>Editar menú</span>
+        </button>
       </div>
     </div>
   );
