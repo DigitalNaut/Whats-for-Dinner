ad0da84e71216f038cf790c6a110ce95490b7e19
John B <johnbernalfsd@gmail.com>
2022-11-11 03:02:12 -0600

    Updated the Roulette component
    - Added a wedge
    - Added a center piece for the results
    - Added wedges with text
    - Added an equation to derive the result
    - Added a random spin function
    Put it all together

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -1,10 +1,16 @@
-import { createRef, useEffect, useRef, useState } from "react";
+import { createRef, useEffect, useState } from "react";
+import { ReactComponent as Wedge } from "src/assets/wedge.svg";
+
+// import history from "src/pages/Main";
+
+const TAU = 2 * Math.PI;
 
 function drawRoulette(
   canvas: HTMLCanvasElement | null,
   offset: number,
   velocity: number,
-  callback: (offset: number) => void
+  onUpdate?: (result: string) => void,
+  onSpinEnd?: (offset: number) => void
 ) {
   if (!canvas) return;
 
@@ -16,7 +22,7 @@ function drawRoulette(
   const radius = halfSize - 4;
 
   context.fillStyle = bgColor;
-  context.ellipse(halfSize, halfSize, radius, radius, 0, 0, 2 * Math.PI);
+  context.ellipse(halfSize, halfSize, radius, radius, 0, 0, TAU);
   context.fill();
 
   const colors = [
@@ -30,56 +36,97 @@ function drawRoulette(
     "#c026d3",
   ];
 
+  const compositeOffset = offset + 0.5 * Math.PI - Math.PI / colors.length;
   colors.forEach((color, index) => {
-    const startAngle = (index * 2 * Math.PI) / colors.length + offset;
-    const endAngle = ((index + 1) * 2 * Math.PI) / colors.length + offset;
+    const startAngle = (index * TAU) / colors.length + compositeOffset;
+    const endAngle = ((index + 1) * TAU) / colors.length + compositeOffset;
 
     context.fillStyle = color;
     context.beginPath();
     context.moveTo(halfSize, halfSize);
     context.arc(halfSize, halfSize, radius, startAngle, endAngle);
     context.fill();
+
+    context.save();
+    context.fillStyle = "white";
+    context.font = "bold 32px sans-serif";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.translate(
+      halfSize + radius * 0.8 * Math.cos(startAngle + Math.PI / colors.length),
+      halfSize + radius * 0.8 * Math.sin(startAngle + Math.PI / colors.length)
+    );
+    context.rotate(startAngle + Math.PI / colors.length + Math.PI / 2);
+    context.fillText(`${index + 1}`, 0, 0);
+    context.restore();
   });
 
-  velocity = velocity < 0.0005 ? 0 : velocity * 0.99;
-  offset = (offset + velocity) % (2 * Math.PI);
-
-  console.log(velocity, offset);
+  velocity = velocity < 0.005 ? 0 : velocity * 0.99;
+  offset = (offset + velocity) % TAU;
 
   const animation = requestAnimationFrame(() => {
-    if (velocity <= 0) {
-      cancelAnimationFrame(animation);
-      return callback(offset);
+    // (Circle unit + 180 degrees for the top wedge)
+    // - the offset to reverse the numbers
+    // + half a wedge width,
+    // all % adjusted for 360 degrees, divided by 360 degrees
+
+    if (onUpdate) {
+      const result =
+        Math.floor(
+          (((Math.PI * 3 - offset + Math.PI / colors.length) % TAU) / TAU) *
+            colors.length
+        ) + 1;
+
+      onUpdate(result.toString());
     }
 
-    drawRoulette(canvas, offset, velocity, callback);
+    if (velocity > 0)
+      drawRoulette(canvas, offset, velocity, onUpdate, onSpinEnd);
+    else {
+      cancelAnimationFrame(animation);
+      onSpinEnd?.(offset);
+    }
   });
 }
 
 export default function Roulette() {
   const rouletteRef = createRef<HTMLCanvasElement>();
-  const velocity = useRef<number>(0);
   const [offset, setOffset] = useState(0);
   const [isSpinning, setIsSpinning] = useState(false);
+  const [result, setResult] = useState("");
 
   useEffect(() => {
-    drawRoulette(rouletteRef.current, offset, velocity.current, (newOffset) => {
-      setOffset(newOffset);
-    });
+    drawRoulette(rouletteRef.current, offset, 0);
   }, []);
 
-  useEffect(() => {
-    if (!isSpinning) return;
+  function randomVelocity(base: number, range: number) {
+    return ((Math.random() * range + base) * Math.PI) / 180;
+  }
 
-    drawRoulette(rouletteRef.current, offset, velocity.current, (newOffset) => {
-      setOffset(newOffset);
-      setIsSpinning(false);
-    });
-  }, [isSpinning]);
+  const spin = () => {
+    if (isSpinning) return;
+
+    setIsSpinning(true);
+
+    drawRoulette(
+      rouletteRef.current,
+      offset,
+      randomVelocity(5, 15),
+      setResult,
+      (newOffset) => {
+        setOffset(newOffset);
+        setIsSpinning(false);
+      }
+    );
+  };
 
   return (
     <div className="w-full">
-      <div className="w-96 aspect-square max-w-full rounded-full bg-white m-auto">
+      <div className="relative w-96 aspect-square max-w-full rounded-full bg-white m-auto">
+        <div className="absolute rounded-full flex justify-center items-center bg-slate-700 w-1/3 aspect-square inset-0 m-auto text-7xl">
+          {result || "No result"}
+        </div>
+        <Wedge className="absolute inset-x-1/2 -inset-y-4 -translate-x-1/2 -translate-y-4" />
         <canvas
           className="w-full h-full aspect-square"
           ref={rouletteRef}
@@ -87,15 +134,8 @@ export default function Roulette() {
           height="400"
         />
       </div>
-      <button
-        data-filled
-        disabled={isSpinning}
-        onClick={() => {
-          velocity.current = ((Math.random() * 15 + 5) * Math.PI) / 180;
-          setIsSpinning(true);
-        }}
-      >
-        Girar
+      <button data-filled disabled={isSpinning} onClick={spin}>
+        {isSpinning ? "Girando" : "Girar"}
       </button>
     </div>
   );
