5acdc983f7f66b483887252d1fe3ac269a6ebd4f
John B <johnbernalfsd@gmail.com>
2022-11-11 14:35:46 -0600

    Refactored Roulette component
    Turned functional code into object-based code

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -1,123 +1,192 @@
-import { createRef, useEffect, useState } from "react";
-import { ReactComponent as Wedge } from "src/assets/wedge.svg";
+import { createRef, useEffect, useRef, useState } from "react";
+import { ReactComponent as Arrow } from "src/assets/wedge.svg";
 
 // import history from "src/pages/Main";
 
 const TAU = 2 * Math.PI;
 
-function drawRoulette(
-  canvas: HTMLCanvasElement | null,
-  offset: number,
-  velocity: number,
-  onUpdate?: (result: string) => void,
-  onSpinEnd?: (offset: number) => void
-) {
-  if (!canvas) return;
+const colors = [
+  "#dc2626",
+  "#d97706",
+  "#65a30d",
+  "#059669",
+  "#0891b2",
+  "#2563eb",
+  "#7c3aed",
+  "#c026d3",
+];
 
-  const context = canvas.getContext("2d") as CanvasRenderingContext2D;
-  context.clearRect(0, 0, canvas.width, canvas.height);
+class SpinnerWedge {
+  private readonly textAngle: number;
+  constructor(
+    public readonly startAngle: number,
+    public readonly endAngle: number,
+    public readonly color: string,
+    public readonly origin: { x: number; y: number }
+  ) {
+    this.textAngle = (startAngle + endAngle) * 0.5;
+  }
 
-  const bgColor = "#334155";
-  const halfSize = canvas.width * 0.5;
-  const radius = halfSize - 4;
-
-  context.fillStyle = bgColor;
-  context.ellipse(halfSize, halfSize, radius, radius, 0, 0, TAU);
-  context.fill();
-
-  const colors = [
-    "#dc2626",
-    "#d97706",
-    "#65a30d",
-    "#059669",
-    "#0891b2",
-    "#2563eb",
-    "#7c3aed",
-    "#c026d3",
-  ];
-
-  const compositeOffset = offset + 0.5 * Math.PI - Math.PI / colors.length;
-  colors.forEach((color, index) => {
-    const startAngle = (index * TAU) / colors.length + compositeOffset;
-    const endAngle = ((index + 1) * TAU) / colors.length + compositeOffset;
-
-    context.fillStyle = color;
+  drawShape(context: CanvasRenderingContext2D, radius: number, angle: number) {
+    context.save();
+    context.fillStyle = this.color;
     context.beginPath();
-    context.moveTo(halfSize, halfSize);
-    context.arc(halfSize, halfSize, radius, startAngle, endAngle);
+    context.moveTo(this.origin.x, this.origin.y);
+    context.arc(
+      this.origin.x,
+      this.origin.y,
+      radius,
+      this.startAngle + angle,
+      this.endAngle + angle
+    );
     context.fill();
+    context.restore();
+  }
 
+  drawText(
+    context: CanvasRenderingContext2D,
+    text: string,
+    origin: { x: number; y: number },
+    angle: number
+  ) {
     context.save();
     context.fillStyle = "white";
-    context.font = "bold 32px sans-serif";
+    context.font = "bold 20px sans-serif";
     context.textAlign = "center";
     context.textBaseline = "middle";
-    context.translate(
-      halfSize + radius * 0.8 * Math.cos(startAngle + Math.PI / colors.length),
-      halfSize + radius * 0.8 * Math.sin(startAngle + Math.PI / colors.length)
-    );
-    context.rotate(startAngle + Math.PI / colors.length + Math.PI / 2);
-    context.fillText(`${index + 1}`, 0, 0);
+
+    context.translate(origin.x, origin.y);
+    context.rotate(angle + this.textAngle);
+    context.fillText(text, 0, 0);
     context.restore();
+  }
+}
+
+class SpinnerWheel {
+  private readonly wedges: SpinnerWedge[] = [];
+  private spinAngle = 0;
+  private angleOffset = 0.5 * Math.PI;
+  private readonly context: CanvasRenderingContext2D;
+
+  constructor(
+    private readonly canvas: HTMLCanvasElement,
+    private readonly origin: { x: number; y: number },
+    private readonly radius: number,
+    private readonly margin: number
+  ) {
+    this.context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.wedges = [];
+  }
+
+  addWedge(wedge: SpinnerWedge) {
+    this.wedges.push(wedge);
+  }
+
+  draw() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.wedges.forEach((wedge) =>
+      wedge.drawShape(
+        this.context,
+        this.radius - this.margin,
+        this.spinAngle + this.angleOffset
+      )
+    );
+
+    const offset = this.spinAngle + this.angleOffset + Math.PI / colors.length;
+
+    this.wedges.forEach((wedge, index) => {
+      const text = (index + 1).toString();
+      const theta = wedge.startAngle + offset;
+      const x = this.origin.x + this.radius * 0.8 * Math.cos(theta);
+      const y = this.origin.y + this.radius * 0.8 * Math.sin(theta);
+      wedge.drawText(this.context, text, { x, y }, this.spinAngle);
+    });
+  }
+
+  spin(
+    velocity: number,
+    onUpdate?: (result: string) => void,
+    onSpinEnd?: () => void
+  ) {
+    this.spinAngle = (this.spinAngle + velocity) % TAU;
+    this.draw();
+
+    velocity = velocity < 0.005 ? 0 : velocity * 0.99;
+
+    const animation = requestAnimationFrame(() => {
+      // (Circle unit + 180 degrees for the top wedge)
+      // - the offset to reverse the numbers
+      // + half a wedge width,
+      // all % adjusted for 360 degrees, divided by 360 degrees
+
+      if (onUpdate) {
+        const result =
+          Math.floor(
+            (((Math.PI * 3 - this.spinAngle) % TAU) / TAU) * colors.length
+          ) + 1;
+
+        onUpdate(result.toString());
+      }
+
+      if (velocity > 0) this.spin(velocity, onUpdate, onSpinEnd);
+      else {
+        cancelAnimationFrame(animation);
+        onSpinEnd?.();
+      }
+    });
+  }
+}
+
+function createRouletteWheel(
+  canvas: HTMLCanvasElement,
+  radius: number,
+  origin: { x: number; y: number }
+) {
+  const wheel = new SpinnerWheel(canvas, origin, radius, 4);
+
+  const wedgeCount = colors.length;
+  const wedgeAngle = TAU / wedgeCount;
+
+  colors.forEach((color, index) => {
+    const startAngle = index * wedgeAngle;
+    const endAngle = startAngle + wedgeAngle;
+    const wedge = new SpinnerWedge(startAngle, endAngle, color, origin);
+    wheel.addWedge(wedge);
   });
 
-  velocity = velocity < 0.005 ? 0 : velocity * 0.99;
-  offset = (offset + velocity) % TAU;
-
-  const animation = requestAnimationFrame(() => {
-    // (Circle unit + 180 degrees for the top wedge)
-    // - the offset to reverse the numbers
-    // + half a wedge width,
-    // all % adjusted for 360 degrees, divided by 360 degrees
-
-    if (onUpdate) {
-      const result =
-        Math.floor(
-          (((Math.PI * 3 - offset + Math.PI / colors.length) % TAU) / TAU) *
-            colors.length
-        ) + 1;
-
-      onUpdate(result.toString());
-    }
-
-    if (velocity > 0)
-      drawRoulette(canvas, offset, velocity, onUpdate, onSpinEnd);
-    else {
-      cancelAnimationFrame(animation);
-      onSpinEnd?.(offset);
-    }
-  });
+  return wheel;
 }
 
 export default function Roulette() {
-  const rouletteRef = createRef<HTMLCanvasElement>();
-  const [offset, setOffset] = useState(0);
+  const canvasRef = createRef<HTMLCanvasElement>();
+  const wheelRef = useRef<SpinnerWheel>();
   const [isSpinning, setIsSpinning] = useState(false);
   const [result, setResult] = useState("");
 
   useEffect(() => {
-    drawRoulette(rouletteRef.current, offset, 0);
+    if (!canvasRef.current) return;
+
+    wheelRef.current = createRouletteWheel(canvasRef.current, 200, {
+      x: canvasRef.current?.width * 0.5,
+      y: canvasRef.current?.height * 0.5,
+    });
+    wheelRef.current.draw();
   }, []);
 
   function randomVelocity(base: number, range: number) {
     return ((Math.random() * range + base) * Math.PI) / 180;
   }
 
-  const spin = () => {
+  const spinTheWheel = () => {
     if (isSpinning) return;
 
     setIsSpinning(true);
 
-    drawRoulette(
-      rouletteRef.current,
-      offset,
-      randomVelocity(5, 15),
-      setResult,
-      (newOffset) => {
-        setOffset(newOffset);
-        setIsSpinning(false);
-      }
-    );
+    const velocity = randomVelocity(10, 10);
+    wheelRef.current?.spin(velocity, setResult, () => {
+      setIsSpinning(false);
+    });
   };
 
   return (
@@ -126,15 +195,15 @@ export default function Roulette() {
         <div className="absolute rounded-full flex justify-center items-center bg-slate-700 w-1/3 aspect-square inset-0 m-auto text-7xl">
           {result || "No result"}
         </div>
-        <Wedge className="absolute inset-x-1/2 -inset-y-4 -translate-x-1/2 -translate-y-4" />
+        <Arrow className="absolute inset-x-1/2 -inset-y-4 -translate-x-1/2 -translate-y-4" />
         <canvas
           className="w-full h-full aspect-square"
-          ref={rouletteRef}
+          ref={canvasRef}
           width="400"
           height="400"
         />
       </div>
-      <button data-filled disabled={isSpinning} onClick={spin}>
+      <button data-filled disabled={isSpinning} onClick={spinTheWheel}>
         {isSpinning ? "Girando" : "Girar"}
       </button>
     </div>
