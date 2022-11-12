121b201cff5cb3e1329f7690323445780ba1c3e7
John B <johnbernalfsd@gmail.com>
2022-11-12 09:13:21 -0600

    Using "label" instead of "name" for choices

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,75 +3,85 @@ import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 import SpinningWheel from "src/components/SpinningWheel";
 
-const history: { name: string; imageUrl: string }[] = [
+export const history: { label: string; imageUrl: string }[] = [
   {
-    name: "Tacos al pastor",
+    label: "Tacos al pastor",
     imageUrl:
       "https://images.unsplash.com/photo-1551504734-5ee1c4a1479b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Pozole", // c-spell-checker:disable-line
+    label: "Pozole", // c-spell-checker:disable-line
     imageUrl:
       "https://images.unsplash.com/photo-1649532245300-c3ed0565ffa4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Ensalada de atún",
+    label: "Ensalada de atún",
     imageUrl:
       "https://images.unsplash.com/photo-1612949060306-4c298ad7f34c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Sopa de pollo",
+    label: "Sopa de pollo",
     imageUrl:
       "https://images.unsplash.com/photo-1569058242276-0bc3e078cf86?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Ensalada verde",
+    label: "Ensalada verde",
     imageUrl:
       "https://images.unsplash.com/photo-1608032077018-c9aad9565d29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Carne asada",
+    label: "Carne asada",
     imageUrl:
       "https://images.unsplash.com/photo-1612871689353-cccf581d667b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Pizza",
+    label: "Pizza",
     imageUrl:
       "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Hamburguesa",
+    label: "Hamburguesa",
     imageUrl:
       "https://images.unsplash.com/photo-1572802419224-296b0aeee0d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Pollo empanizado", // c-spell-checker:disable-line
+    label: "Pollo empanizado", // c-spell-checker:disable-line
     imageUrl:
       "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
   {
-    name: "Arroz con pollo",
+    label: "Arroz con pollo",
     imageUrl:
       "https://images.unsplash.com/photo-1569058242252-623df46b5025?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    label: "Sushi",
+    imageUrl:
+      "https://images.unsplash.com/photo-1553621042-f6e147245754?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
+  },
+  {
+    label: "Spagetti",
+    imageUrl:
+      "https://images.unsplash.com/photo-1635264685671-739e75e73e0f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=256&q=80",
   },
 ];
 
 export default function Main() {
   return (
     <div className="flex flex-col w-full gap-8">
-      <SpinningWheel />
+      <SpinningWheel choices={history} />
 
       <div className="flex p-2 gap-4 min-w-full overflow-x-auto bg-slate-700 ">
         {history.map((dish) => (
-          <div className="group" key={dish.name}>
+          <div className="group" key={dish.label}>
             <div className="relative w-16 md:w-24 lg:w-28 aspect-square rounded-lg bg-gray-700 overflow-hidden">
               <span className="absolute text-center text-sm w-full h-full m-auto hidden group-hover:block bg-black/50 pointer-events-none">
-                {dish.name}
+                {dish.label}
               </span>
               <img
                 className="w-full h-full object-cover"
                 src={dish.imageUrl}
-                alt={dish.name}
+                alt={dish.label}
               />
             </div>
           </div>
