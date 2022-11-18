91d2f82f564a6d93ae41ade376ccb3712011d155
John B <johnbernalfsd@gmail.com>
2022-11-18 01:41:21 -0600

    Added ability to upload menu changes on a delay

--- src/hooks/SpinnerMenuContext.tsx
+++ src/hooks/SpinnerMenuContext.tsx
@@ -6,9 +6,13 @@ import {
   useContext,
   createContext,
 } from "react";
+import Spinner from "src/components/Spinner";
 
 import type { SpinnerOption } from "src/components/SpinningWheel";
 import { useGoogleDrive } from "src/hooks/GoogleDriveContext";
+
+const TIMEOUT = 2500;
+const CONFIG_FILE_NAME = "config.json";
 
 const spinnerMenuContext = createContext<{
   allMenuItems?: SpinnerOption[];
@@ -33,44 +37,90 @@ export function SpinnerMenuContextProvider({ children }: PropsWithChildren) {
     fetchFile,
     fetchList,
     uploadFile,
+    updateFile,
     isLoaded: isDriveLoaded,
   } = useGoogleDrive();
   const [allMenuItems, setAllMenuItems] = useState<SpinnerOption[]>();
-  const [enabledMenuItems, setSpinnerMenuItems] = useState<SpinnerOption[]>();
+  const [enabledMenuItems, setEnabledMenuItems] = useState<SpinnerOption[]>();
+  const [configFileId, setConfigFileId] = useState<string>();
+  const [uploadTimeout, setUploadTimeout] = useState<NodeJS.Timeout>();
+  const [isUploading, setIsUploading] = useState(false);
 
-  const getConfig = useCallback(
+  const setMenuItems = useCallback((config: SpinnerOption[]) => {
+    if (!config) return;
+    setAllMenuItems(config);
+    setEnabledMenuItems(config.filter(({ enabled }) => enabled));
+  }, []);
+
+  const getConfigFileMeta = useCallback(
+    async (signal?: AbortSignal) => {
+      const { data } = await fetchList({
+        signal,
+        params: { q: `name = '${CONFIG_FILE_NAME}'` },
+      });
+
+      // List the files
+      if (!data.files || !data.files.length) return null;
+
+      const [config] = data.files;
+      setConfigFileId(config.id);
+      return config;
+    },
+    [fetchList]
+  );
+
+  const createConfigFile = useCallback(
+    async (signal?: AbortSignal) => {
+      await uploadFile(
+        {
+          file: new File([JSON.stringify([])], CONFIG_FILE_NAME),
+          metadata: {
+            name: "config.json",
+            mimeType: "application/json",
+          },
+        },
+        { signal }
+      );
+    },
+    [uploadFile]
+  );
+
+  const updateConfig = useCallback(async () => {
+    try {
+      if (!configFileId) throw new Error("No config file id");
+
+      await updateFile({
+        id: configFileId,
+        file: new File([JSON.stringify(allMenuItems)], CONFIG_FILE_NAME),
+        metadata: {
+          name: CONFIG_FILE_NAME,
+          mimeType: "application/json",
+        },
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }, [allMenuItems, configFileId, updateFile]);
+
+  const getConfigOrCreate = useCallback(
     async (signal: AbortSignal) => {
       try {
-        const { data } = await fetchList({
-          signal,
-          params: { q: "name = 'config.json'" },
-        });
+        const fileMeta = await getConfigFileMeta();
 
         // List the files
-        if (data.files && data.files.length) {
-          const { data: config } = await fetchFile(data.files[0], {
+        if (fileMeta) {
+          const { data: config } = await fetchFile(fileMeta, {
             signal,
             responseType: "json",
           });
 
           // Set the menu items
           if (config && Array.isArray(config)) {
-            setAllMenuItems(config);
-            setSpinnerMenuItems(config);
+            setMenuItems(config);
           }
-        } else {
-          // Create the config file
-          await uploadFile(
-            {
-              file: new File([JSON.stringify([])], "config.json"),
-              metadata: {
-                name: "config.json",
-                mimeType: "application/json",
-              },
-            },
-            { signal }
-          );
-        }
+        } else createConfigFile();
 
         setIsLoaded(true);
       } catch (error) {
@@ -86,27 +136,45 @@ export function SpinnerMenuContextProvider({ children }: PropsWithChildren) {
         }
       }
     },
-    [fetchFile, fetchList, uploadFile]
+    [createConfigFile, fetchFile, getConfigFileMeta, setMenuItems]
   );
 
-  function updateMenuItems() {
-    if (!allMenuItems) return;
-    setSpinnerMenuItems(allMenuItems.filter(({ enabled }) => enabled));
+  function triggerDelayedUpload(timeout: number = TIMEOUT) {
+    // Reset the timeout
+    if (uploadTimeout) clearTimeout(uploadTimeout);
+
+    const uploadChanges = async () => {
+      // Upload the changes
+      // and toggle the flags
+      setIsUploading(true);
+
+      await updateConfig();
+
+      setUploadTimeout(undefined);
+      setIsUploading(false);
+    };
+
+    // Set the new timeout
+    const timeoutId = setTimeout(uploadChanges, timeout);
+    setUploadTimeout(timeoutId);
   }
 
   function toggleMenuItem(index: number, value?: boolean) {
-    if (!allMenuItems || !enabledMenuItems) return;
+    if (!allMenuItems || !enabledMenuItems) return Promise.resolve(false);
 
     allMenuItems[index].enabled = value ?? !enabledMenuItems[index].enabled;
-    updateMenuItems();
+    setMenuItems(allMenuItems);
+
+    triggerDelayedUpload();
   }
 
   function addMenuItem(item: SpinnerOption) {
-    if (!allMenuItems) return;
+    if (!allMenuItems) return Promise.resolve(false);
 
     allMenuItems.push(item);
-    setAllMenuItems(allMenuItems);
-    updateMenuItems();
+    setMenuItems(allMenuItems);
+
+    triggerDelayedUpload(0);
   }
 
   useEffect(() => {
@@ -115,10 +183,24 @@ export function SpinnerMenuContextProvider({ children }: PropsWithChildren) {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    getConfig(signal);
+    getConfigOrCreate(signal);
 
     return () => controller.abort();
-  }, [getConfig, isDriveLoaded]);
+  }, [getConfigOrCreate, isDriveLoaded]);
+
+  useEffect(() => {
+    const alertUser = (event: BeforeUnloadEvent) => {
+      // See: https://stackoverflow.com/a/69232120/13351497
+      event.preventDefault();
+      return (event.returnValue = "Aún hay información sin guardar");
+    };
+
+    if (!window.onbeforeunload)
+      if (uploadTimeout || isUploading)
+        window.addEventListener("beforeunload", alertUser);
+
+    return () => window.removeEventListener("beforeunload", alertUser);
+  }, [uploadTimeout, isUploading]);
 
   return (
     <spinnerMenuContext.Provider
@@ -130,6 +212,11 @@ export function SpinnerMenuContextProvider({ children }: PropsWithChildren) {
         addMenuItem,
       }}
     >
+      {isUploading && (
+        <div className="fixed inset-x-1/2 top-2 w-fit -translate-x-1/2 rounded-lg bg-emerald-700 p-2 text-white">
+          <Spinner text="Guardando..." />
+        </div>
+      )}
       {error && (
         <div className="w-full rounded-sm bg-red-500 p-2 text-white">
           {error}
