074f7bb4c9b906cc2e80aa099a37896e27e1ce10
John B <johnbernalfsd@gmail.com>
2022-11-24 23:26:51 -0600

    Fix: the Switcher component to works with enums
    Also updated its test file accordingly

--- src/components/Switcher.test.tsx
+++ src/components/Switcher.test.tsx
@@ -2,12 +2,12 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import Switcher from "src/components/Switcher";
+import Switcher, { SwitcherState } from "src/components/Switcher";
 
-it("renders a switcher", () => {
+it("renders a switcher with two options", () => {
   render(
     <Switcher
-      initialState={true}
+      initialState={SwitcherState.FirstOption}
       labels={["first", "second"]}
       renders={{
         firstOption: <p>left option</p>,
@@ -20,12 +20,14 @@ it("renders a switcher", () => {
   const buttons = screen.getAllByRole("button");
 
   expect(buttons).toHaveLength(2);
+  expect(buttons[0]).toHaveTextContent("first");
+  expect(buttons[1]).toHaveTextContent("second");
 });
 
-it("renders the first option", () => {
+it("renders the first option", async () => {
   render(
     <Switcher
-      initialState={true}
+      initialState={SwitcherState.FirstOption}
       labels={["first option", "second option"]}
       renders={{
         firstOption: <p>cats</p>,
@@ -35,15 +37,14 @@ it("renders the first option", () => {
     />
   );
 
-  const firstOption = screen.getByText(/first option/i);
-
-  expect(firstOption).toBeInTheDocument();
+  expect(await screen.findByText(/cats/i)).toBeInTheDocument();
+  expect(screen.queryByText(/dogs/i)).not.toBeInTheDocument();
 });
 
-it("renders the second option", () => {
+it("renders the second option", async () => {
   render(
     <Switcher
-      initialState={false}
+      initialState={SwitcherState.SecondOption}
       labels={["first option", "second option"]}
       renders={{
         firstOption: <p>cats</p>,
@@ -53,17 +54,16 @@ it("renders the second option", () => {
     />
   );
 
-  const secondOption = screen.getByText(/second option/i);
-
-  expect(secondOption).toBeInTheDocument();
+  expect(await screen.findByText(/dogs/i)).toBeInTheDocument();
+  expect(screen.queryByText(/cats/i)).not.toBeInTheDocument();
 });
 
-it("responds to clicks", async () => {
+it("switches display based on clicks", async () => {
   const onChange = jest.fn();
 
   render(
     <Switcher
-      initialState={true}
+      initialState={SwitcherState.FirstOption}
       labels={["first option", "second option"]}
       renders={{
         firstOption: <p>cats</p>,
@@ -76,15 +76,20 @@ it("responds to clicks", async () => {
   const firstOption = screen.getByRole("button", { name: /first option/i });
   const secondOption = screen.getByRole("button", { name: /second option/i });
 
+  expect(screen.getByText(/cats/i)).toBeInTheDocument();
+  expect(screen.queryByText(/dogs/i)).not.toBeInTheDocument();
+
   await userEvent.click(secondOption);
 
   expect(onChange).toHaveBeenCalledTimes(1);
-  expect(onChange).toHaveBeenCalledWith(false);
-  expect(screen.getByText(/dogs/i)).toBeInTheDocument();
+  expect(onChange).toHaveBeenCalledWith(SwitcherState.SecondOption);
+  expect(await screen.findByText(/dogs/i)).toBeInTheDocument();
+  expect(screen.queryByText(/cats/i)).not.toBeInTheDocument();
 
   await userEvent.click(firstOption);
 
   expect(onChange).toHaveBeenCalledTimes(2);
-  expect(onChange).toHaveBeenCalledWith(true);
-  expect(screen.getByText(/cats/i)).toBeInTheDocument();
+  expect(onChange).toHaveBeenCalledWith(SwitcherState.FirstOption);
+  expect(await screen.findByText(/cats/i)).toBeInTheDocument();
+  expect(screen.queryByText(/dogs/i)).not.toBeInTheDocument();
 });
