7b94073a6c90254a8e578735c1c49b067083672d
John B <johnbernalfsd@gmail.com>
2022-11-28 18:47:26 -0600

    Added test suite for Kilobytes component

---
+++ src/components/Kilobytes.test.tsx
@@ -0,0 +1,33 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Kilobytes from "src/components/Kilobytes";
+
+it("renders a custom element with formatted value in bytes as kilobytes", async () => {
+  const { rerender } = render(<Kilobytes value={1024} />);
+
+  expect(await screen.findByText("1 KB"));
+
+  rerender(<Kilobytes value={1024 * 1024} />);
+
+  expect(await screen.findByText("1 MB"));
+
+  rerender(<Kilobytes value={1024 ** 2} />);
+
+  expect(await screen.findByText("1 MB"));
+
+  rerender(<Kilobytes value={1024 ** 3} />);
+
+  expect(await screen.findByText("1 GB"));
+
+  rerender(<Kilobytes value={1024 ** 4} />);
+
+  expect(await screen.findByText("1 TB"));
+
+  rerender(<Kilobytes value={0} />);
+
+  expect(await screen.findByText("0 Bytes"));
+
+  rerender(<Kilobytes value={undefined} />);
+
+  expect(await screen.findByText("0 Bytes"));
+});
